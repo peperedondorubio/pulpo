d675db1a565c5e695b893942daff466a6f0f65ac
peperedondorubio@gmail.com <peperedondorubio@gmail.com>
2025-03-26 15:58:28 +0100

    setup y requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="0.1",
+    version="1.1",
     packages=find_packages(),
     install_requires=[
         "requests",  # Agrega aquí tus dependencias
