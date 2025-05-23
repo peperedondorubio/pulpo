73b9aac3deb70edca8a81818f2f84725d869ef29
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-05-24 00:03:20 +0200

    prueba

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.2",
+    version="1.2.1",
     packages=find_packages(),
     install_requires=[
         "requests",  # Agrega aquí tus dependencias
