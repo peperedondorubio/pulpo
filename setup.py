82502bf7aa4af608f0c295a2cc123b45cfa6b0e3
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-05-23 23:07:53 +0200

    formato de la version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.2_main",
+    version="1.2",
     packages=find_packages(),
     install_requires=[
         "requests",  # Agrega aquí tus dependencias
