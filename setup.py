6d214065f35b0dedb8dfd2226a22e58a948464a8
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-06-23 16:21:45 +0200

    deepndencias

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.3.3",
+    version="1.3.4",
     packages=find_packages(),
     install_requires=[
         "requests", 
         "pyjwt",
-        "aiokafka"
+        "aiokafka",
+        "python-arango"
     ],
 )
