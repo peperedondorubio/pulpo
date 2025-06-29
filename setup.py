acb7be00a7841c1f084a842b8c61a0289c0ecd6c
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-06-29 10:02:45 +0200

    y pongo la version 1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.5.8",
+    version="1.6.0",
     packages=find_packages(),
     install_requires=[
         "requests", 
